a2bf6d7b2c5920febb004986f9f6e6cf21bb0a69
chaitra845 <chaitrachytra@gmail.com>
2021-06-28 10:21:46 +0530

    Added Stock Account Management

---
+++ StockService/StockServiceInterface.java
@@ -0,0 +1,12 @@
+package StockService;
+
+import StockModel.Stock;
+import java.util.HashMap;
+
+public interface StockServiceInterface {
+    public abstract HashMap<String, Stock> addStock(Stock stock, HashMap<String, Stock> stockList);
+    public abstract HashMap<String, Stock> editStock(HashMap<String, Stock> stockList);
+    public abstract void viewStock(HashMap<String, Stock> stockList);
+    public abstract HashMap<String, Stock> deketeStock(HashMap<String, Stock> stockList);
+
+}
